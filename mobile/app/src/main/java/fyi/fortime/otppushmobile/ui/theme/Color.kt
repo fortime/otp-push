394bfa2d00a2d955019a5b0d0e55bfc255df86ae
fortime <palfortime@gmail.com>
2026-05-22 17:28:24 +0800

    feat: update color theme of mobile
    Signed-off-by: fortime <palfortime@gmail.com>

--- mobile/app/src/main/java/fyi/fortime/otppushmobile/ui/theme/Color.kt
+++ mobile/app/src/main/java/fyi/fortime/otppushmobile/ui/theme/Color.kt
@@ -2,10 +2,37 @@ package fyi.fortime.otppushmobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BrandBlue = Color(0xFF2962FF)
+val BrandBackground = Color(0xFFF5F7FA)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light Theme Colors
+val PrimaryLight = BrandBlue
+val OnPrimaryLight = Color.White
+val PrimaryContainerLight = Color(0xFFDBE1FF)
+val OnPrimaryContainerLight = Color(0xFF00174B)
+
+val SecondaryLight = Color(0xFF585E71)
+val OnSecondaryLight = Color.White
+val SecondaryContainerLight = Color(0xFFDDE1F9)
+val OnSecondaryContainerLight = Color(0xFF151B2C)
+
+val BackgroundLight = BrandBackground
+val OnBackgroundLight = Color(0xFF1B1B1F)
+val SurfaceLight = BrandBackground
+val OnSurfaceLight = Color(0xFF1B1B1F)
+
+// Dark Theme Colors
+val PrimaryDark = Color(0xFFB4C5FF)
+val OnPrimaryDark = Color(0xFF00297A)
+val PrimaryContainerDark = Color(0xFF003DAB)
+val OnPrimaryContainerDark = Color(0xFFDBE1FF)
+
+val SecondaryDark = Color(0xFFC1C6DD)
+val OnSecondaryDark = Color(0xFF2A3042)
+val SecondaryContainerDark = Color(0xFF404659)
+val OnSecondaryContainerDark = Color(0xFFDDE1F9)
+
+val BackgroundDark = Color(0xFF1B1B1F)
+val OnBackgroundDark = Color(0xFFE4E2E6)
+val SurfaceDark = Color(0xFF1B1B1F)
+val OnSurfaceDark = Color(0xFFE4E2E6)
--- mobile/app/src/main/java/fyi/fortime/otppushmobile/ui/theme/Theme.kt
+++ mobile/app/src/main/java/fyi/fortime/otppushmobile/ui/theme/Theme.kt
@@ -11,32 +11,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryDark,
+    onPrimary = OnPrimaryDark,
+    primaryContainer = PrimaryContainerDark,
+    onPrimaryContainer = OnPrimaryContainerDark,
+    secondary = SecondaryDark,
+    onSecondary = OnSecondaryDark,
+    secondaryContainer = SecondaryContainerDark,
+    onSecondaryContainer = OnSecondaryContainerDark,
+    background = BackgroundDark,
+    onBackground = OnBackgroundDark,
+    surface = SurfaceDark,
+    onSurface = OnSurfaceDark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryLight,
+    onPrimary = OnPrimaryLight,
+    primaryContainer = PrimaryContainerLight,
+    onPrimaryContainer = OnPrimaryContainerLight,
+    secondary = SecondaryLight,
+    onSecondary = OnSecondaryLight,
+    secondaryContainer = SecondaryContainerLight,
+    onSecondaryContainer = OnSecondaryContainerLight,
+    background = BackgroundLight,
+    onBackground = OnBackgroundLight,
+    surface = SurfaceLight,
+    onSurface = OnSurfaceLight
 )
 
 @Composable
 fun OtpPushMobileTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    // Set to false by default to prefer brand colors
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
